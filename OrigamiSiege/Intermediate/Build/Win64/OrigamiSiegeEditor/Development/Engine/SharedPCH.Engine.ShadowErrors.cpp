e3d788b1b747933a8c22a4d18fb13957ad3d35e0
Chuchocoronel <carlosmegia1@gmail.com>
2022-09-18 22:58:23 +0200

    Basic things

---
+++ OrigamiSiege/Intermediate/Build/Win64/OrigamiSiegeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.32.31329
+#include "E:/Uni/Origami-Siege/OrigamiSiege/Intermediate/Build/Win64/OrigamiSiegeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ OrigamiSiege/Intermediate/Build/Win64/UE4Editor/Inc/OrigamiSiege/Test.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ORIGAMISIEGE_Test_generated_h
+#error "Test.generated.h already included, missing '#pragma once' in Test.h"
+#endif
+#define ORIGAMISIEGE_Test_generated_h
+
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_SPARSE_DATA
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_RPC_WRAPPERS
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUTest(); \
+	friend struct Z_Construct_UClass_UTest_Statics; \
+public: \
+	DECLARE_CLASS(UTest, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/OrigamiSiege"), NO_API) \
+	DECLARE_SERIALIZER(UTest)
+
+
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUTest(); \
+	friend struct Z_Construct_UClass_UTest_Statics; \
+public: \
+	DECLARE_CLASS(UTest, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/OrigamiSiege"), NO_API) \
+	DECLARE_SERIALIZER(UTest)
+
+
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UTest(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UTest) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UTest); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UTest); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UTest(UTest&&); \
+	NO_API UTest(const UTest&); \
+public:
+
+
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UTest(UTest&&); \
+	NO_API UTest(const UTest&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UTest); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UTest); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UTest)
+
+
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_PRIVATE_PROPERTY_OFFSET
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_10_PROLOG
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_PRIVATE_PROPERTY_OFFSET \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_SPARSE_DATA \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_RPC_WRAPPERS \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_INCLASS \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define OrigamiSiege_Source_OrigamiSiege_Test_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_PRIVATE_PROPERTY_OFFSET \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_SPARSE_DATA \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_INCLASS_NO_PURE_DECLS \
+	OrigamiSiege_Source_OrigamiSiege_Test_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ORIGAMISIEGE_API UClass* StaticClass<class UTest>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID OrigamiSiege_Source_OrigamiSiege_Test_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
