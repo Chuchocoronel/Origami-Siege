5a39961d8f7da1c7cf2ce9b8e1cdb1277cf08502
Chuchocoronel <carlosmegia1@gmail.com>
2023-04-17 16:14:12 +0200

    Changes for building an exe

---
+++ OrigamiSiege 5.0/Intermediate/Build/Win64/UnrealGame/Inc/OrigamiSiege/OrigamiSiege.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeOrigamiSiege_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_OrigamiSiege;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_OrigamiSiege()
+	{
+		if (!Z_Registration_Info_UPackage__Script_OrigamiSiege.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/OrigamiSiege",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xB5449644,
+				0x93249D30,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_OrigamiSiege.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_OrigamiSiege.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_OrigamiSiege(Z_Construct_UPackage__Script_OrigamiSiege, TEXT("/Script/OrigamiSiege"), Z_Registration_Info_UPackage__Script_OrigamiSiege, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB5449644, 0x93249D30));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
