2c893fc8933b5f111b1a8d939393f8edd47a2174
Chuchocoronel <carlosmegia1@gmail.com>
2023-06-22 05:23:20 +0200

    More feedback, Win & Lose scene, enemies materials and effects

--- OrigamiSiege 5.0/Intermediate/Build/Win64/OrigamiSiege/Shipping/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ OrigamiSiege 5.0/Intermediate/Build/Win64/OrigamiSiege/Shipping/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.32.31329
+// Compiler: 14.29.30148
 #include "E:/Uni/Origami-Siege/OrigamiSiege 5.0/Intermediate/Build/Win64/OrigamiSiege/Shipping/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ OrigamiSiege 5.0/Intermediate/Build/Win64/OrigamiSiege/Shipping/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.32.31329
+#include "E:/Uni/Origami-Siege/OrigamiSiege 5.0/Intermediate/Build/Win64/OrigamiSiege/Shipping/Engine/SharedPCH.Engine.ShadowErrors.h"
